a7c7d36205a7179469c83a15a5384e025be1f405
Chanida S. <zodality@gmail.com>
2026-05-17 14:11:13 +0700

    fix: use direct googleusercontent API URL

--- utils.js
+++ utils.js
@@ -11,7 +11,7 @@ function displayDate(date) {
   return d.toLocaleDateString();
 }
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbzWOV0oIy2p1tsBhSQNtABhBWKQ1o3TS09JTX7p1IBNCoLqoQ9SptE6jZ8joN0zRPAi/exec';
+const API_URL = 'https://script.googleusercontent.com/macros/echo?user_content_key=AKfycbzWOV0oIy2p1tsBhSQNtABhBWKQ1o3TS09JTX7p1IBNCoLqoQ9SptE6jZ8joN0zRPAi/exec';
 
 async function fetchRows() {
   const res = await fetch(API_URL + '?action=getRows');
